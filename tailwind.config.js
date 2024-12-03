5ef8d426d1d6542685ced491f0fd7df370b080d7
dayeonback <dbaeg257@gmail.com>
2024-12-03 18:18:48 +0900

    12/3 Timer 완성

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'class', // 다크 모드 설정
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
-      mawWidth: {
-        DEFAULT: "1340px",
+      maxWidth: {
+        DEFAULT: '1340px',
       },
       px: {
-        DEFAULT: "1rem",
-        sm: "2rem",
-        md: "4rem",
-        lg: "6rem",
-        xl: "8rem",
+        DEFAULT: '1rem',
+        sm: '2rem',
+        md: '4rem',
+        lg: '6rem',
+        xl: '8rem',
       },
     },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      // 사용자 정의 애니메이션 추가
+      animation: {
+        'fade-in': 'fadeIn 2s ease-in-out forwards', // 페이드 인 애니메이션
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 }, // 시작 상태: 투명
+          '100%': { opacity: 1 }, // 끝 상태: 불투명
+        },
       },
     },
   },
